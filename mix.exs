5e3053f0c498e17261e6b8240a6db75df5d36dc5
Svetlozar Mihailov <svetliomihailov@gmail.com>
2020-08-25 17:55:04 +0300

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pagination.MixProject do
   def project do
     [
       app: :phoenix_pagination,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
